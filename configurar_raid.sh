749c9b5ada48b0d9f7055baaaef83d05c190393a
Javichuck <javichuck@hotmail.es>
2014-12-14 20:13:08 +0100

    Paso de parámetros RAID

---
+++ configurar_raid.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Recopilamos la informacion del fichero de perfil del servicio
+oldIFS=$IFS
+IFS=$'\n'
+C=0
+for linea in $(cat $1); do
+	if [ $C = 0 ]; then
+		#Nombre del dispositivo
+		NOMBRE=$linea
+	elif [ $C = 1 ]; then
+		#Punto de montaje
+		NIVEL=$linea
+	elif [ $C = 2 ]; then
+		#Dispositivos
+		DISPOSITIVOS_AUX=$linea
+	else
+		echo "Error en el formato del fichero de perfil del servicio"
+		exit 1
+	fi
+	let C+=1
+done
+IFS=$' '
+read -a DISPOSITIVOS <<< "DISPOSITIVOS_AUX"
+IFS=$oldIFS
+
+#Numero de dispositivos = ${#DISPOSITIVOS[*]}
+#Dispositivo i = S{DISPOSITIVOS[i]}
