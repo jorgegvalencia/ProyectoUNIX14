97c39e323e2233a14524ee5e69bfc0f5a3d3d0bb
Jorge González Valencia <jorgegvalencia@gmail.com>
2015-06-23 15:59:32 +0200

    Script configurar_cluster.sh actualizado

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -1,19 +1,19 @@
-﻿#!/bin/bash
+#!/bin/bash
 
-# Comprobacion argumentos correctos
+#Comprobacion argumentos correctos
 if [ $# -ne 1 ]
 then
 	echo "Uso: $0 <fichero_configuracion>"
 	exit 1
 fi
 
-# Comprobar que se tienen los ficheros necesarios
+#Comprobar que se tienen los ficheros necesarios
 scripts="configurar_montaje.sh configurar_raid.sh configurar_lvm.sh configurar_nis_server.sh configurar_nis_client.sh configurar_nfs_server.sh configurar_nfs_client.sh configurar_backup_server.sh configurar_backup_client.sh"
 
 for fich in $scripts; do
 	if [ ! -f ./conf/$fich ]
 	then
-		echo "El fichero ./conf/"$fich" no se ha podido encontrar. Abortando ejecución"
+		echo "El fichero ./conf/"$fich" no esta disponible. Abortando ejecución."
 		exit 1
 	fi
 done
@@ -23,119 +23,69 @@ done
 #Contador = 2 (Fichero perfil configuración)
 C=0
 
-# Fichero de configuracion sin lineas no validas (# y blancas)
-#CONFIG=`grep -v '[[:blank:]]*#' $1 | grep '[[:blank:]]'`
+#Fichero de configuración sin líneas no válidas (# y blancas)
+CONFIG=`grep -v '[[:blank:]]*#' $1 | grep '[[:blank:]]'`
 
-#Se analiza cada palabra del fichero de configuración ($1)
-for arg in $(cat $1); do
+for arg in $CONFIG; do
 	if [ $C = 0 ];
 	then
 		#Máquina destino
 		MAQUINA=$arg
-		
 	elif [ $C = 1 ];
 	then
 		#Nombre servicio
 		SERVICIO=$arg
-		
+		case $SERVICIO in
+		mount )
+			SCRIPT=configurar_montaje.sh
+			;;
+		raid )
+			SCRIPT=configurar_raid.sh
+			;;
+		lvm )
+			SCRIPT=configurar_lvm.sh
+			;;
+		nis_server )
+			SCRIPT=configurar_nis_server.sh
+			;;
+		nis_client )
+			SCRIPT=configurar_nis_client.sh
+			;;
+		nfs_server )
+			SCRIPT=configurar_nfs_server.sh
+			;;
+		nfs_client )
+			SCRIPT=configurar_nfs_client.sh
+			;;
+		backup_server )
+			SCRIPT=configurar_backup_server.sh
+			;;
+		backup_client )
+			SCRIPT=configurar_backup_client.sh
+			;;
+		esac
 	elif [ $C = 2 ];
 	then
 		#Ruta del fichero perfil configuración
 		FCONF=$arg
-		oldIFS=$IFS
-		IFS=$'/'
-		read -a FNAME_AUX <<< "$FCONF"
-		IFS=$oldIFS
-		N=${#FNAME_AUX[*]}
-		#Nombre del fichero
-		FNAME=${FNAME_AUX[$(($N-1))]}
-		#Creamos el directorio donde alojaremos los ficheros en la máquina
-		ssh $MAQUINA mkdir /ASI2014
-		case $SERVICIO in
-		mount )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_montaje.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_montaje.sh
-			ssh $MAQUINA /ASI2014/configurar_montaje.sh /ASI2014/$FNAME
-			;;
-		raid )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_raid.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_raid.sh
-			ssh $MAQUINA /ASI2014/configurar_raid.sh /ASI2014/$FNAME
-			;;
-		lvm )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_lvm.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_lvm.sh
-			ssh $MAQUINA /ASI2014/configurar_lvm.sh /ASI2014/$FNAME
-			;;
-		nis_server )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_nis_server.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_nis_server.sh
-			ssh $MAQUINA /ASI2014/configurar_nis_server.sh /ASI2014/$FNAME
-			;;
-		nis_client )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_nis_client.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_nis_client.sh
-			ssh $MAQUINA /ASI2014/configurar_nis_client.sh /ASI2014/$FNAME
-			;;
-		nfs_server )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_nfs_server.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_nfs_server.sh
-			ssh $MAQUINA /ASI2014/configurar_nfs_server.sh /ASI2014/$FNAME
-			;;
-		nfs_client )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_nfs_client.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_nfs_client.sh
-			ssh $MAQUINA /ASI2014/configurar_nfs_client.sh /ASI2014/$FNAME
-			;;
-		backup_server )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_backup_server.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_backup_server.sh
-			ssh $MAQUINA /ASI2014/configurar_backup_server.sh /ASI2014/$FNAME
-			;;
-		backup_client )
-			#Copiamos archivos a la máquina
-			scp ./conf/configurar_backup_client.sh root@$MAQUINA:/ASI2014/ >> /dev/null
-			scp $FCONF root@$MAQUINA:/ASI2014/ >> /dev/null
-			#Ejecutamos el servicio
-			ssh $MAQUINA chmod +x /ASI2014/configurar_backup_client.sh
-			ssh $MAQUINA /ASI2014/configurar_backup_client.sh /ASI2014/$FNAME
-			;;
-		esac
-		#Eliminamos todos los datos de la máquina
-		ssh $MAQUINA rm -r /ASI2014
+		echo 'Fichero de perfil de configuración: '$FCONF
+		#Creamos el directorio temporal donde situaremos los ficheros de configuración
+		echo 'Preparando archivos...'
+		ssh root@$MAQUINA 'mkdir ~/ASI2014/'
+		scp $FCONF root@$MAQUINA:~/ASI2014/$FCONF
+		scp ./conf/$SCRIPT root@$MAQUINA:~/ASI2014/$SCRIPT
+		#Ejecutamos el servicio
+		echo 'Configurando servicio...'
+		ssh root@$MAQUINA "chmod +x ~/ASI2014/$SCRIPT"
+		ssh root@$MAQUINA "~/ASI2014/$SCRIPT ~/ASI2014/$FCONF"
+		#Eliminamos los ficheros de configuración temporales utilizados
+		ssh root@$MAQUINA 'rm -r ~/ASI2014/'
 	else
 		#Error
-		echo 'Error al aumentar el contador'
-		exit 1
+		echo 'ERROR en el fichero de configuración: Formato incorrecto.'
 	fi
-	
-	#Se incrementa el contador
+	#Se aumenta el contador y se calcula el módulo
 	let C+=1
 	let C%=3
-	
 done
 
